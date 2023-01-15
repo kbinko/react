0a5ab6c6a44afed474e586a28863ad1bd4d83164
Kbinko <59764236+kbinko@users.noreply.github.com>
2023-01-15 13:35:13 +0000

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-import Item from './Item.js';
+import React from 'react';
+// import Item from './Item.js';
+
+class StarWars extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Star Wars</h1>
+        <p>These are the Star Wars movies:</p>
+        <ul>
+          <li>A New Hope</li>
+          <li>The Empire Strikes Back</li>
+          <li>Return of the Jedi</li>
+          <li>The Phantom Menace</li>
+          <li>Attack of the Clones</li>
+          <li>Revenge of the Sith</li>
+          <li>The Force Awakens</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 
-
-
+  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Item name="Gully" />
+        {/* <Item name="Gully" />
         <Item name="Konrad" />
-        <Item name="Dupa" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Item name="Dupa" /> */}
+        <StarWars />
+       
       </header>
     </div>
   );
--- my-app/src/Item.js
+++ my-app/src/Item.js
@@ -2,15 +2,42 @@ import React from 'react';
 
 class Item extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicks: 0,
+            totalRemaining: 20
+        };
+    }
+
+
+
     clickMe() {
-        alert("Hello from " + this.props.name);
+        this.setState({
+            clicks: this.state.clicks + 1,
+            totalRemaining: this.state.totalRemaining - 1
+        });
+        if (this.state.totalRemaining === 0) {
+            alert("You spent all your clicks!");
+            this.setState({
+                clicks: 0,
+                totalRemaining: 20
+            });
+        }
+        
     }
 
     render() { 
         return (
-            <h1 onClick={() => this.clickMe()}>
-                Hello from {this.props.name}
-            </h1>
+
+            <div>
+                <h1 onClick={() => this.clickMe()}>
+                    Hello from {this.props.name}
+                </h1>
+                <span>{this.props.name} was clicked {this.state.clicks} times</span>
+                <br />
+                <span>You have {this.state.totalRemaining} clicks left</span>
+            </div>
         );
     }
 }
